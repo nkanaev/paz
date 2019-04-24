d4ac2d1f8a6ed707efb43834a1517a1676fade03
Nazar Kanaev <nkanaev@live.com>
2019-04-24 21:38:29 +0100

    docs

--- paz/__init__.py
+++ paz/__init__.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import os
 import re
 import shutil
@@ -28,69 +29,104 @@ FILE_TYPES = {
 class p(str):
     @property
     def owner(self):
+        """Path owner string."""
         return pwd.getpwuid(os.lstat(self).st_uid).pw_name
 
     @property
     def group(self):
+        """Path group string."""
         return grp.getgrgid(os.lstat(self).st_gid).gr_name
 
     @property
     def last_accessed(self):
+        """File's last accessed date."""
         return datetime.datetime.fromtimestamp(os.path.getatime(self))
 
     @property
     def last_modified(self):
+        """File's last modified date."""
         return datetime.datetime.fromtimestamp(os.path.getmtime(self))
 
     @property
     def exists(self):
+        """True if path exists."""
         return os.path.exists(self)
 
     @property
     def is_dir(self):
+        """True if path is a directory."""
         return os.path.isdir(self)
 
     @property
     def is_file(self):
+        """True if path is a regular file."""
         return os.path.isfile(self)
 
     @property
     def is_link(self):
+        """True if path is a symbolic link."""
         return os.path.islink(self)
 
     @property
     def type(self):
+        """Returns readable file type. Can be one of the following below:
+        'blockdev' / 'chardev' / 'dir' / 'fifo' / 'link' / 'file' / 'socket' / 'unknown'
+        """
         return FILE_TYPES.get(stat.S_IFMT(os.lstat(self).st_mode), 'unknown')
 
     @property
     def fullpath(self):
+        """Returns absolute path, expanding user."""
         return p(os.path.abspath(os.path.expanduser(self)))
 
     @property
     def path(self):
+        """Full path."""
         return self
 
     @property
     def basepath(self):
+        """Full path without extension."""
         return os.path.join(self.dirname, self.basename)
 
     @property
     def dirname(self):
+        """The directory of the path."""
         return os.path.dirname(self)
 
     @property
     def basename(self):
+        """The file name, without extension."""
         return os.path.splitext(os.path.basename(self))[0]
 
     @property
     def filename(self):
+        """The file name of the path."""
         return os.path.basename(self)
 
     @property
     def ext(self):
+        """File extension"""
         return os.path.splitext(self)[1][1:]
 
     def pathmap(self, pattern):
+        """Returns a new path by substituting curly brace delimited path parts in a pattern.
+        The supported path parts are illustrated below:
+
+        /home/username/pics/portrait-of-madame-x.png
+
+        └────────┬────────┘ └─────────┬────────┘ └┬┘
+              dirname              basename      ext
+
+                           └───────────┬──────────┘
+                                   filename
+
+        └───────────────────┬──────────────────┘
+                        basepath
+
+        └──────────────────────┬───────────────────┘
+                             path
+        """
         newpath = ''
         for part in re.split(r'({\w+})', pattern):
             if part.startswith('{') and part.endswith('}'):
@@ -103,12 +139,16 @@ class p(str):
         return p(newpath)
 
     def hash(self, name):
+        """Hash string of the file using the provided hash algorithm."""
         hasher = getattr(hashlib, name)()
         with self.open('rb') as f:
             hasher.update(f.read())
             return hasher.hexdigest()
 
     def find(self, include=None, exclude=None, type=None):
+        """Returns a generator which recursively descends the directory tree
+        yielding matching paths.
+        """
         for root, dirs, files in os.walk(self):
             for sub in [dirs, files]:
                 for n in sub:
@@ -122,33 +162,45 @@ class p(str):
                     yield newp
 
     def __truediv__(self, other):
+        """Join path with another path/string."""
         return p(os.path.join(self, other))
 
     def __rtruediv__(self, other):
+        """Left join path with another path/string."""
         return p(os.path.join(other, self))
 
     def copy(self, pattern, copy_metadata=False, follow_symlinks=True):
+        """Copies file to a new target.
+        Target string is substituted according to ``pathmap`` rules.
+        """
         newp = self.pathmap(pattern)
         copyfn = shutil.copy if not copy_metadata else shutil.copy2
         copyfn(self, newp, follow_symlinks=follow_symlinks)
         return newp
 
     def move(self, pattern, copy_function=None):
+        """Moves file to a new target.
+        Target string is substituted according to ``pathmap`` rules.
+        """
         newp = self.pathmap(pattern)
         shutil.move(self, newp, copy_function=copy_function)
         return newp
 
     def open(self, *args, **kwargs):
+        """Wrapper around ``open``."""
         return open(self, *args, **kwargs)
 
     def chdir(self):
+        """Wrapper around ``os.chdir``."""
         # TODO: context processor
         os.chdir(self)
 
     def chmod(self, mode, **kwargs):
+        """Wrapper around ``os.chmod``."""
         os.chmod(self, mode, **kwargs)
 
     def chown(self, owner=None, group=None):
+        """Change path owner."""
         if not owner and not group:
             raise Exception('provide either "owner" or "group"')
         uid, gid = owner, group
